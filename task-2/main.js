7c3727e48975a2524ac0e4bbe1d3f6010646698b
Avtandil Kakulia <avtokakulia@gmail.com>
2024-10-31 03:14:57 +0400

    Resolve Task-2 first and second parts

---
+++ task-2/main.js
@@ -0,0 +1,46 @@
+// Task-1. მოცემულია მასივი [{name: 'Temo', age: 25}, {name: 'Lasha', age: 21}, {name: 'Ana', age: 28}]. დაწერე ფუნქცია, რომელიც პარამეტრად მიიღებს user - ების მასივს და დააბრუნებს ყველაზე პატარა ასაკის მქონე ადამიანის სახელს.
+
+// const userArr = [{ name: 'Temo', age: 25 }, { name: 'Lasha', age: 21 }, { name: 'Ana', age: 28 }]
+
+// function nameByAge(arr) {
+//     let userName = arr[0].name
+//     let minAge = arr[0].age
+
+//     for (const item of arr) {
+//         if (item.age < minAge) {
+//             minAge = item.age
+//             userName = item.name
+//         }
+//     }
+
+//     return userName
+
+// }
+
+// console.log(nameByAge(userArr) + ' is most young!');
+
+// Task-2. დაწერე ფუნქცია, რომელიც პარამეტრად მიიღებს user ობიექტს და დააბრუნებს იგივე მნიშვნელობების მქონე ახალ (განსხვავებულ) ობიექტს.
+
+// const user = {
+//     name: 'jack',
+//     lastName: 'Smith',
+//     age: 30,
+//     isMale: true,
+//     smile: function () {
+//         console.log('Jack can smile.');
+//     }
+// }
+
+// function createCopy(obj){
+//     let objCopy = {...obj}
+//     objCopy.name = 'david' // გავჩეკოთ რომ განსხვავებულია
+
+//     return objCopy
+// }
+
+// console.log(user);
+// console.log(createCopy(user));
+
+
+
+
