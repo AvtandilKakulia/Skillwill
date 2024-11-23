285aa6f8b7034ac7a8d8923bc3a880b6abbbb814
Avtandil Kakulia <avtokakulia@gmail.com>
2024-11-23 16:55:01 +0400

    Update Task-8 part one

--- task-8/task 8-1/main.js
+++ task-8/task 8-1/main.js
@@ -1,8 +1,7 @@
 /*
-დაწერე ფუნქცია expo, რომელიც იქნება რეკურსიული ფუნქცია და მიიღებს არგუმენტად:
+დაწერე ფუნქცია, რომელიც იქნება რეკურსიული ფუნქცია და მიიღებს არგუმენტად:
 ა) ციფრს ბ) ხარისხს და გ) callback - ს და დააბრუნებს მიღებული ციფრის ხარისხს მაგალითად: 5 ხარისხად 3 - არის 125 (5 * 5 *5)
 */
-
 
 function powerOf(num, pow, cb) {
     if (pow === 0) return cb(1)
@@ -11,13 +10,17 @@ function powerOf(num, pow, cb) {
         powerOf(num, pow - 1, (result) => {
             cb(result * num)
         })
+    } else {
+        powerOf(num, pow + 1, (result) => {
+            cb(result / num)
+        })
     }
-
 }
 
 powerOf(2, 3, (res) => {
-    console.log(res);
+    console.log(res)
 })
+
 
 
 
